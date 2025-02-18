ea82e3d110954b18b415fab965f98644a92d4804
Goncalves Killian <killiangoncalves@hotmail.com>
2025-02-18 20:54:56 +0100

    Form css pour login register et reset de pwd

--- sources/views/register/index.php
+++ sources/views/register/index.php
@@ -4,35 +4,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../dist/css/main.css">
     <title>Inscription</title>
 </head>
 
 <body>
-    <h2>Inscription</h2>
 
-    <?php if (!empty($errors)): ?>
-        <div style="color: red;">
-            <ul>
-                <?php foreach ($errors as $error): ?>
-                    <li><?= htmlspecialchars($error) ?></li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
-    <?php endif; ?>
 
-    <form method="POST" action="/register">
+    <form class="form" method="POST" action="/register">
+
+        <h2>Inscription</h2>
+
+        <?php if (!empty($errors)): ?>
+            <?php foreach ($errors as $error): ?>
+                <p class="form__error-message"><?= htmlspecialchars($error) ?></p>
+            <?php endforeach; ?>
+        <?php endif; ?>
+
         <label>Email :</label>
-        <input type="email" name="email" required>
-        
+        <div class="form__input-wrapper">
+            <input type="email" name="email" placeholder="Entrez votre adresse e-mail" required>
+        </div>
+
         <label>Nom d'utilisateur :</label>
-        <input type="text" name="username" required>
-        
+        <div class="form__input-wrapper">
+            <input type="text" name="username" placeholder="Entrez votre nom d'uttilisateur" required>
+        </div>
+
         <label>Mot de passe :</label>
-        <input type="password" name="password" required>
-        
+        <div class="form__input-wrapper">
+            <input type="password" name="password" placeholder="Entrez votre mot de passe"required>
+        </div>
+
         <label>Confirmer le mot de passe :</label>
-        <input type="password" name="password_confirm" required>
-        
+        <div class="form__input-wrapper">
+            <input type="password" name="password_confirm" placeholder="Confirmer votre mot de passe"required>
+        </div>
+
         <button type="submit">S'inscrire</button>
     </form>
 </body>
