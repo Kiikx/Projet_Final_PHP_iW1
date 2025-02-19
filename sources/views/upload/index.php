3339b55247d3fec950385ec059ceafdb21015165
Iness Ben <ine.benss@gmail.com>
2025-02-19 01:49:49 +0100

    feature: ajout d'une fonctionalité drag & drop

--- sources/views/upload/index.php
+++ sources/views/upload/index.php
@@ -4,22 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload d'image</title>
+    <link rel="stylesheet" href="/dist/css/main.css">
 </head>
 <body>
-    <h2>Upload une image</h2>
+    <h2 class="title">Publiez votre PIPIcs</h2>
     
-    <form action="/upload" method="POST" enctype="multipart/form-data">
+    <form id="upload-form" action="/upload" method="POST" enctype="multipart/form-data" class="drag-uploader">
         <label for="group_id">ID du groupe :</label>
         <input type="number" name="group_id" required>
         
         <br><br>
 
-        <label for="image">Sélectionne une image :</label>
-        <input type="file" name="image" accept="image/jpeg, image/png, image/gif, image/webp" required>
+        <div class="drag-uploader__drop-area" id="drop-area">
+            <p>Glissez & déposez votre image ici ou <label for="file-input" class="drag-uploader__label"><strong>cliquez pour sélectionner<strong></label></p>
+            <input type="file" id="file-input" name="image" accept="image/jpeg, image/png, image/gif, image/webp" class="drag-uploader__file-input" required>
+            <div class="drag-uploader__preview-container" id="preview-container"></div>
+        </div>
 
-        <br><br>
-
-        <button type="submit">Uploader</button>
+        <button class="button" type="submit">Envoyer</button>
     </form>
+
+    <script src="/src/js/dragUploader.js"></script>
 </body>
 </html>
