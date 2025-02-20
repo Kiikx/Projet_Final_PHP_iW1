ccd8ce3d0cd9aa15fd68a7594ab7ad38bfe46146
Iness Ben <ine.benss@gmail.com>
2025-02-20 22:43:22 +0100

    dark mode : on progress

--- sources/index.php
+++ sources/index.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../dist/css/main.css">
+    <title>Tripipics</title>
+</head>
+<body>
+    <button class="button" id="theme-toggle">🌙</button>
+    <script src="../src/js/main.js" defer></script>
+
+</body>
+</html>
+
 <?php
 session_start();
 
